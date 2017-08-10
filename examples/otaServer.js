c3ea4bf5d73f640ce3ee14c00c04e0863c635fce
Andras Fekete <andras.fekete@silabs.com>
2017-08-10 17:10:28 -0400

    Valid response to an OTA query

--- examples/otaServer.js
+++ examples/otaServer.js
@@ -20,15 +20,23 @@ otaCluster.on('queryNextImage', function(request, response) {
   console.log('Image Type: ', request.imageType);
   console.log('File Version: ', request.fileVersion);
   console.log('Image Size: ', request.imageSize);
+	console.log('size=', getFilesizeInBytes("1234-5678-80000000.ota"));
 
   response.send({
-    status: 0x0002,
-    manufacturerCode: 0x0002,
-    imageType: 0x0002,
-    fileVersion: 0x0002,
-    imageSize: 0x0002
+    status: 0x0000,
+    manufacturerCode: request.manufacturerCode,
+    imageType: request.imageType,
+    fileVersion: request.fileVersion + 1,
+    imageSize: getFilesizeInBytes("1234-5678-80000000.ota")
   });
 });
+
+const fs = require("fs");
+function getFilesizeInBytes(filename) {
+    const stats = fs.statSync(filename)
+    const fileSizeInBytes = stats.size
+    return fileSizeInBytes
+}
 
 otaCluster.listen(coapServer);
 
