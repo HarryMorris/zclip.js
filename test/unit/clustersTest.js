220eb7dcaeb48ee1d424c6cbbeef5be87db7c86d
Lee Byrd <Lee.Byrd@silabs.com>
2017-09-08 13:50:03 -0400

    Cluster classes created with Cluster base class

--- test/unit/clustersTest.js
+++ test/unit/clustersTest.js
@@ -16,12 +16,15 @@ test('Builds clusters from meta data', () => {
   expect(target.OnOffCluster).toBeDefined();
 });
 
-test('Adds metadata to clusters', () => {
+test('Adds metadata to cluster instances', () => {
   var target = {};
 
   var clusters = require(__appRoot + 'lib/clusters')(clusterMetaData);
   clusters.build(target);
 
-  expect(target.OnOffCluster.meta).toBeDefined();
-  expect(target.OnOffCluster.meta.code).toEqual('0x0006');
+  var onOffCluster = new target.OnOffCluster();
+
+  expect(onOffCluster.meta).toBeDefined();
+  expect(onOffCluster.meta.code).toEqual('0x0006');
 });
+
