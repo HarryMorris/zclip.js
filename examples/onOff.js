97fdd6540d872b270349d0f38615afb311b2bf46
Lee Byrd <Lee.Byrd@silabs.com>
2017-09-15 16:51:10 -0400

    cleaning up examples; create on-off example

---
+++ examples/onOff.js
@@ -0,0 +1,29 @@
+var coap = require('coap');
+var zcl = require('../.')(coap);
+
+var onOffCluster = new zcl.OnOffCluster({
+  ip: '2001:db8:385:9318:db95:a7b8:e803:73af',
+  endpoint: 1
+});
+
+setTimeout(function() {
+  console.log('Sending on');
+  onOffCluster.on();
+}, 0);
+
+setTimeout(function() {
+  console.log('Sending off');
+  onOffCluster.off();
+}, 1000);
+
+setTimeout(function() {
+  console.log('Sending toggle');
+  onOffCluster.toggle();
+}, 2000);
+
+setTimeout(function() {
+  console.log('Sending off');
+  onOffCluster.off();
+}, 3000);
+
+
