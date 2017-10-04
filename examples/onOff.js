16e787f77acc0398ed98abaee78254a029304f5e
Lee Byrd <Lee.Byrd@silabs.com>
2017-10-04 16:52:15 -0400

    Remove node_modules from source control

--- examples/onOff.js
+++ examples/onOff.js
@@ -2,7 +2,7 @@ var coap = require('coap');
 var zcl = require('../.')(coap);
 
 var onOff = new zcl.clusters.OnOff({
-  ip: '2001:db8:385:9318:db95:a7b8:e803:73af',
+  ip: '2001:db8:385:9318:f001:951a:2aa1:cbf7',
   endpoint: 1
 });
 
