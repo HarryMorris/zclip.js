f81f0b924665cf8d4e3abc4e935ace76909ff368
Lee Byrd <Lee.Byrd@silabs.com>
2018-02-24 10:16:48 -0500

    Pulls read into Cluster

--- test/ClusterTest.js
+++ test/ClusterTest.js
@@ -1,10 +1,13 @@
 require(__dirname + '/support/testHelper');
 
+
 describe('Cluster', () => {
+  var AttributeCollection;
   var Cluster;
   var coap;
 
   beforeAll(() => {
+    AttributeCollection = require(__appRoot + 'lib/AttributeCollection');
     Cluster = require(__appRoot + 'lib/Cluster');
   });
 
@@ -43,6 +46,89 @@ describe('Cluster', () => {
     expect(cluster.side).toEqual('s');
     expect(cluster.rdIp).toEqual('2001::2');
     expect(cluster.rdPort).toEqual(5685);
+  });
+
+  describe('read', function() {
+    test('sends an attribute request', () => {
+      var cluster = Cluster({
+        clusterId: '6',
+        ip: '::1',
+        port: 5684,
+        endpoint: 2,
+        side: 's'
+      }, coap);
+
+
+      cluster.read();
+
+      expect(coap.lastRequest).toBeDefined();
+      expect(coap.lastRequest.params.hostname).toEqual('::1');
+      expect(coap.lastRequest.params.port).toEqual(5684);
+      expect(coap.lastRequest.params.method).toEqual('GET');
+      expect(coap.lastRequest.params.pathname).toEqual('/zcl/e/2/s6/a');
+      expect(coap.lastRequest.params.query).toEqual('f=*');
+      expect(coap.lastRequest.ended).toBeTruthy();
+    });
+
+    test('calls back with decoded attributes', (done) => {
+      var attributeCollection = AttributeCollection({
+        0: { name: 'sceneCount' },
+        1: { name: 'currentScene' }
+      });
+
+      var cluster = Cluster({
+        clusterId: '5',
+        ip: '::1',
+        attributeCollection: attributeCollection
+      }, coap);
+
+      cluster.read({}, (err, response, code) => {
+        expect(coap.lastRequest).toBeDefined();
+        expect(response.sceneCount).toEqual(2);
+        expect(response.currentScene).toEqual(1)
+        done();
+      });
+
+      var responsePayload = new Map();
+      responsePayload.set(0, { v: 2 });
+      responsePayload.set(1, { v: 1 });
+
+      coap.lastRequest.sendResponse({
+        code: '2.04',
+        payload: cbor.encode(responsePayload)
+      });
+    });
+
+    test('decodes empty response', function(done) {
+      var cluster = Cluster({
+        clusterId: '5',
+        ip: '::1'
+      }, coap);
+
+      cluster.read({}, (err, response, code) => {
+        expect(response).toEqual({});
+        done();
+      });
+
+      coap.lastRequest.sendResponse({
+        code: '4.04',
+        payload: cbor.encode('')
+      });
+    });
+
+    test('calls back with error', function(done) {
+      var cluster = Cluster({
+        clusterId: '5',
+        ip: '::1'
+      }, coap);
+
+      cluster.read({}, (err, response, code) => {
+        expect(err).toEqual('Nope');
+        done();
+      });
+
+      coap.lastRequest.sendError('Nope');
+    });
   });
 
   describe('bind', () => {
@@ -267,227 +353,5 @@ describe('Cluster', () => {
     });
   });
 
-  // describe('read', function() {
-  //   test('sends an attribute request', function(done) {
-  //     var metaData = { code: '0x0006' };
-  //
-  //     var clusterBase = new ClusterBase(metaData, zclip);
-  //     var ip = '192.168.1.1';
-  //     var port = 5683;
-  //     var basePath = '/zcl/e/1/s6/';
-  //
-  //     var clusterBase = new ClusterBase(metaData, zclip);
-  //
-  //     clusterBase.ip = ip;
-  //     clusterBase.port = port;
-  //     clusterBase.basePath = basePath;
-  //
-  //     clusterBase.read({}, function(err, attributes) {
-  //       expect(fakeCoap.lastRequest).toBeDefined();
-  //       expect(fakeCoap.lastRequest.params.hostname).toEqual(ip);
-  //       expect(fakeCoap.lastRequest.params.port).toEqual(port);
-  //       expect(fakeCoap.lastRequest.params.method).toEqual('GET');
-  //       expect(fakeCoap.lastRequest.params.pathname).toEqual('/zcl/e/1/s6/a');
-  //       expect(fakeCoap.lastRequest.params.query).toEqual('f=*');
-  //       expect(fakeCoap.lastRequest.ended).toBeTruthy();
-  //       done();
-  //     });
-  //
-  //     var payload = new Map();
-  //
-  //     payload.set(0, 'foo');
-  //     fakeCoap.lastRequest.sendResponse({
-  //       code: '2.01',
-  //       payload: cbor.encode(payload)
-  //     });
-  //   });
-  //
-  //   test.skip('decodes attribute response', function(done) {
-  //     var metaData = {
-  //       code: '0x0006',
-  //       attributes: {
-  //         0: {
-  //           name: 'attr1'
-  //         },
-  //         1: {
-  //           name: 'attr2'
-  //         }
-  //       }
-  //     };
-  //
-  //     var clusterBase = new ClusterBase(metaData, zclip);
-  //
-  //     var payload = new Map();
-  //     payload.set(0, { v: 100 });
-  //     payload.set(1, { v: 200 });
-  //
-  //     clusterBase.read({}, function(err, result) {
-  //       expect(result.responseCode).toEqual('2.01');
-  //       expect(result.response.attr1).toEqual(100);
-  //       expect(result.response.attr2).toEqual(200);
-  //       done();
-  //     });
-  //
-  //     fakeCoap.lastRequest.sendResponse({
-  //       code: '2.01',
-  //       payload: cbor.encode(payload)
-  //     });
-  //   });
-  //
-  //   test.skip('decodes empty response', function(done) {
-  //     var metaData = {
-  //       code: '0x0006',
-  //       attributes: {
-  //         0: {
-  //           name: 'attr1'
-  //         },
-  //         1: {
-  //           name: 'attr2'
-  //         }
-  //       }
-  //     };
-  //
-  //     var clusterBase = new ClusterBase(metaData, zclip);
-  //
-  //     clusterBase.read({}, function(err, result) {
-  //       expect(result.responseCode).toEqual('4.04');
-  //       expect(result.response).toEqual('');
-  //       done();
-  //     });
-  //
-  //     fakeCoap.lastRequest.sendResponse({
-  //       code: '4.04',
-  //       payload: new Buffer('')
-  //     });
-  //   });
-  //
-  //   test.skip('calls back with error', function(done) {
-  //     var metaData = {
-  //       code: '0x0006',
-  //       attributes: {
-  //         0: {
-  //           name: 'attr1'
-  //         },
-  //         1: {
-  //           name: 'attr2'
-  //         }
-  //       }
-  //     };
-  //
-  //     var clusterBase = new ClusterBase(metaData, zclip);
-  //
-  //     clusterBase.read({}, function(err, result) {
-  //       expect(err.message).toEqual('No reply in 3s');
-  //       done();
-  //     });
-  //
-  //     expect(fakeCoap.lastRequest).toBeDefined();
-  //     fakeCoap.lastRequest.sendError({
-  //       message: 'No reply in 3s'
-  //     });
-  //   });
-  // });
 });
 
-//
-// describe('listen', function() {
-//   var coapServer;
-//   var clusterBase;
-//
-//   beforeEach(function() {
-//     var metaData = {
-//       "commands": {
-//         "0": {
-//           "name": "moveToLevel",
-//           "args": {
-//             "0": {
-//               "name": "level",
-//             },
-//             "1": {
-//               "name": "transitionTime",
-//             }
-//           }
-//         }
-//       }
-//     }
-//
-//     clusterBase = new ClusterBase(metaData, zclip);
-//     clusterBase.basePath = '/zcl/e/1/s6/';
-//
-//     coapServer = fakeCoap.createServer();
-//   });
-//
-//   test('fires command handler when coap server receives command', function() {
-//     const moveToLevelHandler = jest.fn();
-//     clusterBase.moveToLevelHandler = moveToLevelHandler;
-//     clusterBase.listen(coapServer);
-//
-//     coapServer.request('/zcl/e/1/s6/c/0');
-//
-//     expect(moveToLevelHandler).toHaveBeenCalled();
-//   });
-//
-//   test('parses payload', function() {
-//     var commandRequest;
-//
-//     clusterBase.moveToLevelHandler = function(request, response) {
-//       commandRequest = request;
-//     };
-//
-//     clusterBase.listen(coapServer);
-//
-//     var payload = new Map();
-//     payload.set(0, '100');
-//     payload.set(1, '5');
-//
-//     coapServer.request('/zcl/e/1/s6/c/0', cbor.encode(payload));
-//
-//     expect(commandRequest.payload.level).toEqual('100');
-//     expect(commandRequest.payload.transitionTime).toEqual('5');
-//   });
-//
-//   test('response.send ends response', function() {
-//     clusterBase.listen(coapServer);
-//     clusterBase.moveToLevelHandler = function(request, response) {
-//       response.send();
-//     };
-//
-//     var request = coapServer.request('/zcl/e/1/s6/c/0');
-//
-//     expect(request.hasEnded()).toBeTruthy();
-//   });
-//
-//
-//   test('ignores requests for other endpoints', function() {
-//     clusterBase.listen(coapServer);
-//
-//     const moveToLevelHandler = jest.fn();
-//     clusterBase.moveToLevelHandler = moveToLevelHandler;
-//
-//     coapServer.request('/zcl/e/2/s6/c/0');
-//
-//     expect(moveToLevelHandler).not.toHaveBeenCalled();
-//   });
-//
-//   test('ignores requests for other clusters', function() {
-//     clusterBase.listen(coapServer);
-//
-//     const moveToLevelHandler = jest.fn();
-//     clusterBase.moveToLevelHandler = moveToLevelHandler;
-//
-//     coapServer.request('/zcl/e/1/s9/c/0');
-//
-//     expect(moveToLevelHandler).not.toHaveBeenCalled();
-//   });
-//
-//   test('ignores unknown commands', function() {
-//     clusterBase.listen(coapServer);
-//
-//     const moveToLevelHandler = jest.fn();
-//     clusterBase.moveToLevelHandler = moveToLevelHandler;
-//
-//     coapServer.request('/zcl/e/1/s6/c/9');
-//
-//     expect(moveToLevelHandler).not.toHaveBeenCalled();
-//   });
-// });
